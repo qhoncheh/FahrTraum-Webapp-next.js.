63e1963c3773df271bb04cb7846a90f8f6097e91
qhoncheh <ghoncheh.ataee@gmail.com>
2025-06-12 08:17:25 +0330

    Hero styles mit tailwindcss

--- components/HeroSection/Hero.tsx
+++ components/HeroSection/Hero.tsx
@@ -6,13 +6,13 @@ const Hero = () => {
   const handleScroll = () => {
   }
   return (
-    <div className=' hero'>
+    <div className=" flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
       <div className='flex-1 padding-x'>
-        <h1 className='hero__title'>
+        <h1 className='2xl:text-[61px] sm:text-[64px] text-[50px] font-extrabold'>
           Finden, buchen oder mieten Sie ein Auto -- schnell
           und einfach!
         </h1>
-        <p className="hero__subtitle">
+        <p className="text-[27px] text-black-100 font-light mt-5">
           Optimieren Sie Ihre Autovermietung
           mit unserem unkomplizierten Buchungsprozess.
         </p>
@@ -22,10 +22,10 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className='hero__image-container'>
-        <div className="hero__image">
+      <div className='xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen'>
+        <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
           <Image src="/hero.png" alt="hero" fill className='object-contain'/>
-        <div className='hero__image-overlay'></div>
+          <div className='absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden'></div>
         </div>
       </div>
     </div>
