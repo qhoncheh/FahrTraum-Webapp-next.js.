579a8fe535be7022cbdadb2f4d753458f7ee6f5a
qhoncheh <ghoncheh.ataee@gmail.com>
2025-06-14 19:33:28 +0330

    Refactor SearchMenu component: enhance manufacturer filtering and improve input styling

--- components/SearchMenu/searchmenu.tsx
+++ components/SearchMenu/searchmenu.tsx
@@ -1,29 +1,76 @@
 "use client"
-import { manufacturers } from "@/constants"
 import { SearchMenuProps } from "@/types"
-import { Combobox , Transition  } from "@headlessui/react"
+import { Combobox, ComboboxOption, Transition } from "@headlessui/react"
 import Image from "next/image"
-  import { useState } from "react"
+import { Fragment, useState } from "react"
+import { manufacturers } from "@/constants"
+import { Volkhov } from "next/font/google"
+
 const SearchMenu = ({ menu, setMenu }: SearchMenuProps) => {
-  const [query, setQuery] = useState()
-  
+  const [query, setQuery] = useState<string>('')
+
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) => (
+        item.toLowerCase()
+        .replace(/\s+/g, ""))
+        .includes(query.toLocaleLowerCase().replace(/\s+/g,
+        "")
+  ))
+
   return (
     <div className="flex-1 max-sm:w-full flex justify-start items-center">
       <Combobox>
         <div className="relative w-full">
-          <Combobox.Button className="absolute top-[14px]">
+          <Combobox.Button className="absolute left-4 top-[2px] z-10">
             <Image src="/car-logo.svg"
               width={20}
               height={20}
               alt="Auto logo"
-              className="ml-4"
             />
           </Combobox.Button>
-          <Combobox.Input className="search-manufacture__input" 
+          <Combobox.Input
+            className="search-manufacture__input pl-12"
             placeholder="Volkwagen"
             displayValue={(manufacturer: string) => manufacturer}
             onChange={(e) => setQuery(e.target.value)}
           />
+          <Transition as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-0"
+            afterLeave={() => setQuery('')}
+          >
+            <Combobox.Options>
+                 { filteredManufacturers.map((item) => (
+                    <ComboboxOption
+                      key={item}
+                      value={item}
+                      className={({ active }) =>
+                        `relative search-manufacturer__option ${active ? 'bg-primary-blue' : 'text-gray-900'}`
+                      }
+                    >
+                 {({ selected, active }) => (
+                   <>
+                     <span className={`block truncate ${
+                       selected ? 'font-medium' : 'font-normal'}`}
+                     >
+                       {item}
+                     </span>
+                     {selected ? (
+                       <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                         active ? 'text-white' : 'text-teal-600'}`}
+                       >
+                       </span>
+                     ) : null}
+                   </>
+                 )}
+                     </ComboboxOption>
+                  ))
+              }
+            </Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
     </div>
