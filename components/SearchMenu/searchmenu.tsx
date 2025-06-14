6c3913a5e0cd18e59bad42711912bd22d5e062d4
qhoncheh <ghoncheh.ataee@gmail.com>
2025-06-14 15:05:00 +0330

    Update SearchMenu component to display car logo in the search button

--- components/SearchMenu/searchmenu.tsx
+++ components/SearchMenu/searchmenu.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { SearchMenuProps } from "@/types"
 import { Combobox , Transition  } from "@headlessui/react"
+import Image from "next/image"
   
 const SearchMenu = ({ menu, setMenu }: SearchMenuProps) => {
   return (
@@ -8,7 +9,12 @@ const SearchMenu = ({ menu, setMenu }: SearchMenuProps) => {
       <Combobox>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
-            <Image src="/"/>
+            <Image src="/car-logo.svg"
+              width={20}
+              height={20}
+              alt="Auto logo"
+              className="ml-4"
+            />
           </Combobox.Button>
         </div>
       </Combobox>
